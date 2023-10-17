fd274a1827eead61356038908c56f1517b5295fa
Tammy Zhang <tz332@cornell.edu>
2023-10-17 09:35:21 -0400

    implement swr data fetching

--- src/app/components/Controls.js
+++ src/app/components/Controls.js
@@ -1,38 +1,6 @@
 // component holding the user input controls for the visualization
 import Select from './Select.js'
 import styles from '../page.module.css'; 
-import * as d3 from 'd3';
-import useSWR from 'swr'
-
-// formatting dropdown options for Model Run
-let modelOpts = [];
-const fetcher = (url) => fetch(url).then((res) => res.text());
-
-function formatDates(date_str) {
-    // Returns: none. Changes value of modelOpts variable to an array of formatted strings representing model run timestamps.
-    // Parameter date_str: the data returned from a SWR fetch request to a csv file holding available dates.
-    let valid_init_hours=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
-    let run_date_strs = date_str.split("\n");
-    let dates = [];
-    for (let i=0; i< run_date_strs.length; i++) {
-        dates.push(new Date(Date.UTC(parseInt(run_date_strs[i].substring(0, 4)),
-                            parseInt(run_date_strs[i].substring(4, 6)) -1,
-                            parseInt(run_date_strs[i].substring(6, 8)),
-                            parseInt(run_date_strs[i].substring(8, 10)),
-                            parseInt(run_date_strs[i].substring(10, 12)))
-        ));
-    }
-    dates = dates.filter(function(d) {return valid_init_hours.includes(d.getUTCHours());});
-    dates = dates.reverse();
-        
-    // formatting for time and date in the dropdown
-    const formatTimeValue = d3.utcFormat("%Y%m%d%H%M");
-    const formatTimeLabel = d3.utcFormat("%Y %b %d %H%M UTC");
-
-    // loop that adds valid dates/times to the dropdown
-    let dates_final = dates.map(rd => formatTimeLabel(rd))
-    modelOpts = dates_final
-}
 
 //// formatting dropdown options for Ensemble Members
 let ensembleNum = Array.from(Array(18).keys()) // creating option for ensemble members 1-18
@@ -53,22 +21,14 @@ let forecastOpts = forecastIntervals.map(function(interval) {
     return `${hours}:${mins}`}
 )
 
-export default function Controls() {
-    console.log("render occurred! Controls")
-
-    // const url = 'https://wofsdltornado.blob.core.windows.net/wofs-dl-preds/available_dates.csv';
-    const url = "/available_dates.csv"
-    const { data, error } = useSWR(url, fetcher, {revalidateOnFocus: false});
-
-    if (data) {
-        formatDates(data);
-    }
+export default function Controls({ modelDates }) {
+    console.log("render occurred! Controls") 
    
     return (
         <div className={styles.controls}>
             <Select 
                 label={"Model Run"} 
-                options={modelOpts}
+                options={modelDates}
             />
             <Select 
                 label={"Ensemble Member"} 
--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,49 @@
 import styles from './page.module.css'
 import Controls from './components/Controls.js'
 import Visualizations from './components/Visualizations.js'
+import useSWR from 'swr'
+import * as d3 from 'd3';
+
+const fetcher = (url) => fetch(url).then((res) => res.text());
+const url = "/available_dates.csv"
 
 export default function App() {
+  const { data, error, isLoading } = useSWR(url, fetcher, {revalidateOnFocus: false});
+
+  if (error) return <div>failed to load</div>
+  if (isLoading) return <div>loading...</div>
+ 
   return (
       <main>
-          <Controls />
+          <Controls modelDates={formatDates(data)} />
           <Visualizations />
       </main>
   )
+}
+
+function formatDates(date_str) {
+  // Returns: an array of strings representing model run timestamps. Used to populate Model Run dropdown.
+  // Parameter date_str: the data returned from a SWR fetch request to a csv file holding available dates.
+  let valid_init_hours=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+  let run_date_strs = date_str.split("\n");
+  let dates = [];
+  for (let i=0; i< run_date_strs.length; i++) {
+      dates.push(new Date(Date.UTC(parseInt(run_date_strs[i].substring(0, 4)),
+                          parseInt(run_date_strs[i].substring(4, 6)) -1,
+                          parseInt(run_date_strs[i].substring(6, 8)),
+                          parseInt(run_date_strs[i].substring(8, 10)),
+                          parseInt(run_date_strs[i].substring(10, 12)))
+      ));
+  }
+  dates = dates.filter(function(d) {return valid_init_hours.includes(d.getUTCHours());});
+  dates = dates.reverse();
+      
+  // formatting for time and date in the dropdown
+  const formatTimeValue = d3.utcFormat("%Y%m%d%H%M");
+  const formatTimeLabel = d3.utcFormat("%Y %b %d %H%M UTC");
+
+  // loop that adds valid dates/times to the dropdown
+  let datesFinal = dates.map(rd => formatTimeLabel(rd))
+  return datesFinal;
 }
 
